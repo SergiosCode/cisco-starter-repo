aac3ddddf1ec4ecee483a7d99c2b283234898bf9
SergioV <sergioscode@gmail.com>
2022-07-09 02:55:14 -0700

    Created the banner

---
+++ sextant/src/Components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "../styles/Header.module.css";
+
+const Header = () => {
+  return <div className={styles.mainContainer}>
+    <h1>Sextant</h1>
+  </div>;
+};
+
+export default Header;
